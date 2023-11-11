d378816eed52fa06e350eece00f2a7b990c71087
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2023-11-11 19:19:16 -0300

    Aula 3.3

--- scripts/Aula3.3.c
+++ scripts/Aula3.3.c
@@ -3,6 +3,21 @@
 int main(){
 
     int dado = 10;
+    printf("Dado antes do incremento: %d. \n ", dado);
 
-    printf("");
+    dado ++;
+    printf("Dado depois do incremento: %d. \n", dado);
+
+    dado --;
+    printf("Dado depois do decremento %d. \n", dado);
+
+    dado += 3;
+    printf("Dado depois do incremento de 3 unidades: %d. \n", dado);
+
+    dado -= 2;
+    printf("Dado depois do decremento de 2 unidades: %d. \n", dado);
+
+    dado *= 10;
+    printf("Dado depois de ser multiplicado por 10: %d. \n", dado);
+
 }
