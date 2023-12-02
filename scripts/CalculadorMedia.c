b0bd3fa46e736686d51a97d7b9154be65b2b083d
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2023-12-02 20:55:01 -0300

    Melhorando scripts, com conhecimento novo adquirido

--- scripts/CalculadorMedia.c
+++ scripts/CalculadorMedia.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*
 int main(){
     printf("Digite suas Notas 1 2 e 3 \n");
     float n1, n2, n3, soma;
@@ -17,4 +18,34 @@ int main(){
     }else{
         printf("Reprovado \n");
     }  
+}
+*/
+
+void CalculadorM(float n1, float n2, float n3){
+    float m;
+    m = (n1 + n2 + n3) / 3;
+
+    if(m <= 3){
+        printf("Reprovado \n");
+    }else if(m > 3 && m < 6 ){
+        printf("Recuperação \n");
+    }else if(m >= 7){
+        printf("Aprovado \n");
+    }else{
+        printf("Eror");
+    }
+    
+
+}
+
+int main(){
+
+    float n1, n2, n3;
+    printf("Digite suas notas Abaixo \n");
+    scanf("%f", &n1);
+    scanf("%f", &n2);
+    scanf("%f", &n3);
+
+    CalculadorM(n1, n2, n3);
+
 }
