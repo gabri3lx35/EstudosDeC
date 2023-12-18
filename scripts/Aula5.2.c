e7f50f5a93997eb28036fefc20e69d49e8c8019b
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2023-12-18 19:19:55 -0300

    loops and laços

---
+++ scripts/Aula5.2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+int main(){
+
+    int i=1;
+
+    do{
+        printf("%d \n", i);
+        i++;
+    }while(i <= 10);
+}
+
+
+int main(){
+
+ int x;
+
+ do{
+    printf("Digite 0 ou 1");
+    scanf("%d \n",&x);
+
+ }while(x != 0 && x !=1);
+
+ printf("Voce digitou %d", x);
+}
+
+*/
